66be70649a5d3ece6271ca80f1ea0cf244a00945
Naqeeb Ur Rehman <naqeeburrehman.pk@gmail.com>
2024-01-20 20:58:26 +0500

    fix: updated UI

--- pages/coupon/AddCoupon.tsx
+++ pages/coupon/AddCoupon.tsx
@@ -10,113 +10,145 @@ import { tostify } from "@components/common/tostify";
 import { useDispatch } from "react-redux";
 import { Input } from "@mui/material";
 import { toast } from "react-toastify";
+import MUIButton from "@components/common/commonButton";
+import { useTheme } from "@emotion/react";
+import CommonTextField from "@components/common/commonTextField";
 
 const style = {
-    position: "absolute" as "absolute",
-    borderRadius: "20px",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    boxShadow: 24,
-    p: 4,
+  position: "absolute" as "absolute",
+  borderRadius: "20px",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
 };
 
 export default function AddCoupon({ setUpdateCouponsList }: any) {
-    const [open, setOpen] = React.useState(false);
-    const [amount, setAmount]: any = React.useState("");
-    const [duration, setDuration]: any = React.useState("");
-    const [couponCode, setCouponCode] = React.useState("");
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const dispatch = useDispatch();
+  const [open, setOpen] = React.useState(false);
+  const [amount, setAmount]: any = React.useState("");
+  const [duration, setDuration]: any = React.useState("");
+  const [couponCode, setCouponCode] = React.useState("");
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const dispatch = useDispatch();
+  const theme = useTheme();
 
-    const canSave = [amount && amount <= 100 && amount > 0, duration].every(Boolean);
+  const canSave = [amount && amount <= 100 && amount > 0, duration].every(Boolean);
 
-    const _addCoupon = async (e: any) => {
-        if (!canSave) return toast.error("incorrect values");
-        e.preventDefault();
-        const body = {
-            code: couponCode,
-            amount,
-            duration,
-        };
-        try {
-            const res = await dispatch(addCoupon(body));
-            const error = ErrorHandler(res);
-            if (error) {
-                tostify(res?.payload?.message, "success");
-            }
-            setUpdateCouponsList((prev: any) => prev + 1);
-            handleClose();
-            setCouponCode('')
-            setAmount('')
-            setDuration('')
-        } catch (error) {
-            tostify("Something went wrong!", "error");
-        }
+  const _addCoupon = async (e: any) => {
+    if (!canSave) return toast.error("incorrect values");
+    e.preventDefault();
+    const body = {
+      code: couponCode,
+      amount,
+      duration,
     };
+    try {
+      const res = await dispatch(addCoupon(body));
+      const error = ErrorHandler(res);
+      if (error) {
+        tostify(res?.payload?.message, "success");
+      }
+      setUpdateCouponsList((prev: any) => prev + 1);
+      handleClose();
+      setCouponCode("");
+      setAmount("");
+      setDuration("");
+    } catch (error) {
+      tostify("Something went wrong!", "error");
+    }
+  };
 
-    return (
-        <div>
-            <Button onClick={handleOpen}>
-                <Add /> Add New Coupon
-            </Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <CloseOutlined
-                        sx={{ position: "absolute", top: "10px", right: "10px" }}
-                        onClick={handleClose}
-                        style={{ cursor: "pointer" }}
-                    />
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Add new coupon
-                    </Typography>
-                    <br />
-                    <form onSubmit={_addCoupon} className="grid gap-2">
-                        <label className="text-gray-500">Coupon Value</label>
-                        <input
-                            className="border rounded-xl p-2"
-                            value={couponCode}
-                            onChange={(e) => setCouponCode(e.target.value)}
-                            type="text"
-                        />
-                        <label className="text-gray-500">discount percentage (1-100)</label>
-                        <input
-                            className={`${
-                                (amount > 100 || amount < 1) && amount ? "border-red-500" : null
-                            } border rounded-xl p-2`}
-                            min={1}
-                            max={100}
-                            value={amount}
-                            onChange={(e) => setAmount(e.target.value)}
-                            type="text"
-                        />
-                        <label className="text-gray-500">days till valid (in number)</label>
-                        <input
-                            className="border rounded-xl p-2"
-                            value={duration}
-                            onChange={(e) => setDuration(e.target.value)}
-                            type="text"
-                        />
-                        <div className="border rounded-xl mt-4">
-                            <Button
-                                disabled={!canSave}
-                                type="submit"
-                                style={{ whiteSpace: "nowrap", padding: "10px", width: "100%" }}
-                            >
-                                Add Coupon
-                            </Button>
-                        </div>
-                    </form>
-                </Box>
-            </Modal>
-        </div>
-    );
+  return (
+    <div>
+      <MUIButton
+        backgroundColor={theme.palette.action}
+        hoverBgColor={theme.palette.action}
+        fullWidth={false}
+        height="48px"
+        marginTop="20px"
+        marginLeft="20px"
+        marginBottom="20px"
+        text="Add New Coupon"
+        width="auto"
+        borderRadius="50px"
+        onClick={handleOpen}
+      />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <CloseOutlined
+            sx={{ position: "absolute", top: "10px", right: "10px" }}
+            onClick={handleClose}
+            style={{ cursor: "pointer" }}
+          />
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add new coupon
+          </Typography>
+          <br />
+          <form onSubmit={_addCoupon} className="grid gap-2">
+            <CommonTextField
+              text="Coupon Value"
+              placeholder="Please enter new Coupon Code."
+              size="medium"
+              type="text"
+              name="postalCode"
+              width="100%"
+              value={couponCode}
+              onChange={(e: any) => setCouponCode(e.target.value)}
+            />
+            <CommonTextField
+              text="discount percentage"
+              placeholder="Please enter a number between 1-100."
+              size="medium"
+              type="text"
+              name="postalCode"
+              width="100%"
+              value={amount}
+              onChange={(e: any) => {
+                const isValidPostalCode = /^(100|[1-9]?[0-9])$/.test(e.target.value);
+                if (isValidPostalCode || e.target.value === "") {
+                  setAmount(e.target.value);
+                }
+              }}
+            />
+            <CommonTextField
+              text="days till valid"
+              placeholder="Please enter the number of days."
+              size="medium"
+              type="text"
+              name="postalCode"
+              width="100%"
+              value={duration}
+              onChange={(e: any) => {
+                const isValidPostalCode = /^[0-9]+$/.test(e.target.value);
+                if (isValidPostalCode || e.target.value === "") {
+                  setDuration(e.target.value);
+                }
+              }}
+            />
+            <MUIButton
+              disabled={!canSave}
+              type="submit"
+              backgroundColor={theme.palette.action}
+              hoverBgColor={theme.palette.action}
+              fullWidth={false}
+              height="48px"
+              marginTop="20px"
+              text="Add Coupon"
+              width="auto"
+              borderRadius="50px"
+            />
+          </form>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
--- pages/coupon/index.tsx
+++ pages/coupon/index.tsx
@@ -28,219 +28,214 @@ import { formatDateAndTime } from "@redux/utils/formatDate";
 import { Save } from "@mui/icons-material";
 
 const useStyles = makeStyles()((theme) => {
-    return {
-        thead: {
-            "& .MuiTableCell-root": {
-                // padding: "10px !important",
-                border: "none !important",
-                fontSize: "14px !important",
-                whiteSpace: "nowrap",
-            },
-        },
-    };
+  return {
+    thead: {
+      "& .MuiTableCell-root": {
+        // padding: "10px !important",
+        border: "none !important",
+        fontSize: "14px !important",
+        whiteSpace: "nowrap",
+      },
+    },
+  };
 });
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        color: "#0099CB",
-        padding: 10,
-        lineHeight: "24px",
-        fontWeight: 500,
-        fontSize: "14px",
-        background: theme.palette.background.paper,
-        border: "0px",
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 12,
-    },
+  [`&.${tableCellClasses.head}`]: {
+    color: "#0099CB",
+    padding: 10,
+    lineHeight: "24px",
+    fontWeight: 500,
+    fontSize: "14px",
+    background: theme.palette.background.paper,
+    border: "0px",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 12,
+  },
 }));
 
 const coupon = () => {
-    const dispatch = useDispatch();
-    const classes = useStyles();
-    const theme = useTheme();
-    const setFullPageLoader = usePageLoader();
-    const router = useRouter();
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const theme = useTheme();
+  const setFullPageLoader = usePageLoader();
+  const router = useRouter();
 
-    const [updateCouponsList, setUpdateCouponsList] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const [page, setPage] = React.useState<any>(0);
-    const [couponList, setcouponList] = React.useState<any>([]);
-    const [searchText, setSearchText] = React.useState<any>("");
-    const handleChangePage = (newPage: number) => {
-        setPage(newPage);
+  const [updateCouponsList, setUpdateCouponsList] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState<any>(0);
+  const [couponList, setcouponList] = React.useState<any>([]);
+  const [searchText, setSearchText] = React.useState<any>("");
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (value: any) => {
+    setRowsPerPage(value);
+    setPage(0);
+  };
+
+  const _getcoupon = async () => {
+    setFullPageLoader(true);
+    const body: any = {
+      page: page + 1,
+      limit: rowsPerPage,
     };
+    if (searchText) {
+      body.search = searchText;
+    }
+    try {
+      const res = await dispatch(getCoupon(body));
+      const error = ErrorHandler(res);
+      if (error) {
+        setcouponList(res?.payload?.data);
+      }
+      setFullPageLoader(false);
+    } catch (error) {
+      tostify("Something went wrong!", "error");
+      setFullPageLoader(false);
+    }
+  };
 
-    const handleChangeRowsPerPage = (value: any) => {
-        setRowsPerPage(value);
-        setPage(0);
-    };
-
-    const _getcoupon = async () => {
-        setFullPageLoader(true);
-        const body: any = {
-            page: page + 1,
-            limit: rowsPerPage,
-        };
-        if (searchText) {
-            body.search = searchText;
-        }
-        try {
-            const res = await dispatch(getCoupon(body));
-            const error = ErrorHandler(res);
-            if (error) {
-                setcouponList(res?.payload?.data);
-            }
-            setFullPageLoader(false);
-        } catch (error) {
-            tostify("Something went wrong!", "error");
-            setFullPageLoader(false);
-        }
-    };
-
-    useEffect(() => {
-        if (router.isReady) {
-            _getcoupon();
-        }
-    }, [router, rowsPerPage, page, searchText, updateCouponsList]);
-    return (
-        <>
-            <PrivateRoute>
-                <Layout>
-                    <PaperContainer bodyPadding="0px" border>
-                        <LayoutHeader
-                            type="coupon"
-                            paddingLeft={"15px"}
-                            paddingBottom={"10px"}
-                            handleSearch={(val: any) => {
-                                setSearchText(val);
-                            }}
-                            searchBy={["code"]}
+  useEffect(() => {
+    if (router.isReady) {
+      _getcoupon();
+    }
+  }, [router, rowsPerPage, page, searchText, updateCouponsList]);
+  return (
+    <>
+      <PrivateRoute>
+        <Layout>
+          <PaperContainer bodyPadding="0px" border>
+            <AddCoupon setUpdateCouponsList={setUpdateCouponsList} />
+          </PaperContainer>
+          <br />
+          <PaperContainer bodyPadding="0px" border>
+            <LayoutHeader
+              type="coupon"
+              paddingLeft={"15px"}
+              paddingBottom={"10px"}
+              handleSearch={(val: any) => {
+                setSearchText(val);
+              }}
+              searchBy={["code"]}
+            />
+            <TableContainer style={{ marginTop: 0 }}>
+              <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                  {couponList?.length > 0 && (
+                    <TableRow>
+                      <StyledTableCell align="left">Coupon Code</StyledTableCell>
+                      <StyledTableCell align="left">Discount percentage</StyledTableCell>
+                      <StyledTableCell align="left">Expire At</StyledTableCell>
+                      <StyledTableCell align="left">Status</StyledTableCell>
+                    </TableRow>
+                  )}
+                </TableHead>
+                <TableBody>
+                  {/* className={classes.thead} */}
+                  {couponList?.length > 0 ? (
+                    couponList?.map((row: any) => <TableRowComp data={row} />)
+                  ) : (
+                    <TableRow>
+                      <TableCell colSpan={12}>
+                        <NoDataFound
+                          title="No details found!"
+                          variant="body2"
+                          icon={<ErrorOutlineIcon style={{ fontSize: "2rem", color: theme.palette.info.main }} />}
+                          elevation={0}
                         />
-                        <TableContainer style={{ marginTop: 0 }}>
-                            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                                <TableHead>
-                                    {couponList?.length > 0 && (
-                                        <TableRow>
-                                            <StyledTableCell align="left">Coupon Code</StyledTableCell>
-                                            <StyledTableCell align="left">Discount percentage</StyledTableCell>
-                                            <StyledTableCell align="left">Expire At</StyledTableCell>
-                                            <StyledTableCell align="left">Status</StyledTableCell>
-                                        </TableRow>
-                                    )}
-                                </TableHead>
-                                <TableBody>
-                                    {/* className={classes.thead} */}
-                                    {couponList?.length > 0 ? (
-                                        couponList?.map((row: any) => <TableRowComp data={row} />)
-                                    ) : (
-                                        <TableRow>
-                                            <TableCell colSpan={12}>
-                                                <NoDataFound
-                                                    title="No details found!"
-                                                    variant="body2"
-                                                    icon={
-                                                        <ErrorOutlineIcon
-                                                            style={{ fontSize: "2rem", color: theme.palette.info.main }}
-                                                        />
-                                                    }
-                                                    elevation={0}
-                                                />
-                                            </TableCell>
-                                        </TableRow>
-                                    )}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        {couponList?.length > 0 && (
-                            <CustomPagination
-                                count={couponList?.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                                onPageChange={handleChangePage}
-                            />
-                        )}
-                        <AddCoupon setUpdateCouponsList={setUpdateCouponsList} />
-                    </PaperContainer>
-                </Layout>
-            </PrivateRoute>
-        </>
-    );
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            {couponList?.length > 0 && (
+              <CustomPagination
+                count={couponList?.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                onPageChange={handleChangePage}
+              />
+            )}
+          </PaperContainer>
+        </Layout>
+      </PrivateRoute>
+    </>
+  );
 };
 
 const TableRowComp = ({ data }: any) => {
-    const [checked, setChecked] = useState(data.isActive);
-    const [name, setName] = useState(data.code);
-    const [expAt, setExpAt] = useState();
-    const [isEditingName, setIsEditingName] = useState(false);
-    const [isEditingExp, setIsEditingExp] = useState(false);
-    const dispatch = useDispatch();
+  const [checked, setChecked] = useState(data.isActive);
+  const [name, setName] = useState(data.code);
+  const [expAt, setExpAt] = useState();
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingExp, setIsEditingExp] = useState(false);
+  const dispatch = useDispatch();
 
-    const StyledTableRow = styled(TableRow)(({ theme }) => ({
-        "&:nth-of-type(odd)": {
-            backgroundColor: theme.palette.action.hover,
-        },
-        "&:last-child td, &:last-child th": {
-            border: 0,
-        },
-        "& .MuiTableCell-root": {
-            color: "#333333",
-            // padding: "8px !important",
-            border: "none !important",
-            fontSize: "12px !important",
-            fontWeight: "bolder",
-        },
-        "& .mui-1kg957h": {
-            // padding: "0"
-        },
-    }));
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+    "& .MuiTableCell-root": {
+      color: "#333333",
+      // padding: "8px !important",
+      border: "none !important",
+      fontSize: "12px !important",
+      fontWeight: "bolder",
+    },
+    "& .mui-1kg957h": {
+      // padding: "0"
+    },
+  }));
 
-    const _update = async (id: any, isActive: boolean) => {
-        const body = {
-            couponId: id,
-            isActive: `${isActive}`,
-        };
-        setChecked(!checked);
-        try {
-            const res = await dispatch(updateCoupon(body));
-            const error = ErrorHandler(res);
-            if (error) {
-                tostify(res?.payload?.message, "success");
-            }
-        } catch (error) {
-            setChecked(!checked);
-            tostify("Something went wrong!", "error");
-        }
+  const _update = async (id: any, isActive: boolean) => {
+    const body = {
+      couponId: id,
+      isActive: `${isActive}`,
     };
+    setChecked(!checked);
+    try {
+      const res = await dispatch(updateCoupon(body));
+      const error = ErrorHandler(res);
+      if (error) {
+        tostify(res?.payload?.message, "success");
+      }
+    } catch (error) {
+      setChecked(!checked);
+      tostify("Something went wrong!", "error");
+    }
+  };
 
-    return (
-        <StyledTableRow key={data.code}>
-            <StyledTableCell onDoubleClick={() => setIsEditingName(!isEditingName)} align="left">
-                {isEditingName ? (
-                    <Input value={name} onChange={(e) => setName(e.target.value)} />
-                ) : (
-                    <Box>{data?.code}</Box>
-                )}
-            </StyledTableCell>
-            <StyledTableCell align="left">{data?.amount} % off</StyledTableCell>
-            <StyledTableCell align="left">{formatDateAndTime(data?.expAt)}</StyledTableCell>
-            <StyledTableCell align="left">
-                <Switch
-                    color="primary"
-                    sx={{
-                        borderRadius: "6px",
-                        height: "40px",
-                        cursor: "pointer",
-                    }}
-                    checked={checked}
-                    onClick={() => {
-                        _update(data._id, !checked);
-                    }}
-                />
-            </StyledTableCell>
-        </StyledTableRow>
-    );
+  return (
+    <StyledTableRow key={data.code}>
+      <StyledTableCell onDoubleClick={() => setIsEditingName(!isEditingName)} align="left">
+        {isEditingName ? <Input value={name} onChange={(e) => setName(e.target.value)} /> : <Box>{data?.code}</Box>}
+      </StyledTableCell>
+      <StyledTableCell align="left">{data?.amount} % off</StyledTableCell>
+      <StyledTableCell align="left">{formatDateAndTime(data?.expAt)}</StyledTableCell>
+      <StyledTableCell align="left">
+        <Switch
+          color="primary"
+          sx={{
+            borderRadius: "6px",
+            height: "40px",
+            cursor: "pointer",
+          }}
+          checked={checked}
+          onClick={() => {
+            _update(data._id, !checked);
+          }}
+        />
+      </StyledTableCell>
+    </StyledTableRow>
+  );
 };
 
 export default coupon;
